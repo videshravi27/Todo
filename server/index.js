9a3d81c4fb666fb55288254a9a4452dd0f65404b
Videsh Ravi <videshravi27@gmail.com>
2025-05-28 01:59:07 +0530

    added frontend url

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://todo-ivory-three.vercel.app",
     credentials: true,
   })
 );
